555feafae35f1053d3c4e09fc0bd1d14805beb18
FChadni <begumfa1@msu.edu>
2021-02-27 03:15:18 -0500

    initial commit

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Farjana Chadni on 2/27/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
